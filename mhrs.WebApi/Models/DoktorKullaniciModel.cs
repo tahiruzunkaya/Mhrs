729f61d685a320561aff2205baf012244032043e
Tahir Uzunkaya <tahiruzunkaya1@hotmail.com>
2019-05-24 02:40:56 +0300

    modeladd

---
+++ mhrs.WebApi/Models/DoktorKullaniciModel.cs
@@ -0,0 +1,14 @@
+﻿using mhrs.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mhrs.WebApi.Models
+{
+    public class DoktorKullaniciModel
+    {
+        public Doktor doktor { get; set; }
+        public Kullanici kullanici { get; set; }
+    }
+}
