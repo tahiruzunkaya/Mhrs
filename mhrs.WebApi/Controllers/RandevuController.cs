ec08a9a286f92bd55ef958b79552b67e341d799c
Tahir Uzunkaya <tahiruzunkaya1@hotmail.com>
2019-05-24 04:11:13 +0300

    asd

--- mhrs.WebApi/Controllers/RandevuController.cs
+++ mhrs.WebApi/Controllers/RandevuController.cs
@@ -2,14 +2,53 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using mhrs.Data.Abstract;
+using mhrs.WebApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace mhrs.WebApi.Controllers
 {
     [Route("api/[controller]/[action]")]
     [ApiController]
-    public class RandevuController : ControllerBase
+    public class RandevuController : Controller
     {
+
+        private IUnitOfWork uow;
+
+        public RandevuController(IUnitOfWork _uow)
+        {
+            uow = _uow;
+        }
+
+        [HttpGet]
+        public JsonResult TarihControl(string tarih1,int doktorid)
+        {
+            DateTime tarih = DateTime.Parse(tarih1);
+            var result = uow.Randevular.Find(i => (i.Tarihi < (tarih.AddDays(1)) || i.Tarihi > tarih.AddDays(-1)) && i.DoktorId==doktorid).ToList();
+            List<RandevuControlModel> lst = new List<RandevuControlModel>();
+            if (result.Count > 0)
+            {
+
+                foreach (var item in result)
+                {
+                    RandevuControlModel m = new RandevuControlModel()
+                    {
+                        saat = item.Tarihi.Hour.ToString() + ":" + item.Tarihi.Minute
+                    };
+
+                    lst.Add(m);
+
+                }
+
+                
+
+            }
+
+            return Json(JsonConvert.SerializeObject(lst));
+        }
+
+
     }
 }
---
+++ mhrs.WebApi/Models/RandevuControlModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mhrs.WebApi.Models
+{
+    public class RandevuControlModel
+    {
+        public string saat { get; set; }
+    }
+}
